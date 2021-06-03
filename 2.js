02a010a441e48be677080540673a11756c97e7c8
goutam-naik7 <naikgautam257@gmail.com>
2021-06-03 15:40:44 +0530

    problems -2

--- 2.js
+++ 2.js
@@ -1,13 +1,15 @@
 const string = "The quick brown fox jumps over the lazy dog";
 
-function count(string) {
-  var count = [];
-  string.split("").forEach(function (s) {
-    count[s] ? count[s]++ : (count[s] = 1);
-  });
-  return count;
-}
+// function count(string) {
+//   var count = {};
+//   string.split("").forEach(function (s) {
+//     count[s] ? count[s]++ : (count[s] = 1);
+//   });
+//   return count;
+// }
 
-const z = count(string);
+// const z = count(string);
 
-console.log(z);
+const counter = (s) => [...s].reduce((a, c) => (a[c] = a[c] + 1 || 1) && a, {});
+
+console.log(counter(string));
